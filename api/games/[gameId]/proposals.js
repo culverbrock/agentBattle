1a3e7cbc824ba4cf84eb159f51fc058a8394422c
Brock Culver <brock.culver@gmail.com>
2025-05-22 18:13:13 -0400

    Add CORS headers to all API endpoints and update frontend index.html

--- api/games/[gameId]/proposals.js
+++ api/games/[gameId]/proposals.js
@@ -7,6 +7,13 @@
 const proposalManager = require('../../../proposalManager.js');
 
 module.exports = async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  if (req.method === 'OPTIONS') {
+    res.status(200).end();
+    return;
+  }
   if (req.method !== 'POST') {
     res.status(405).json({ error: 'Method not allowed' });
     return;
--- api/games/[gameId]/votes.js
+++ api/games/[gameId]/votes.js
@@ -7,6 +7,13 @@
 const pool = require('../../../database.js');
 
 module.exports = async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  if (req.method === 'OPTIONS') {
+    res.status(200).end();
+    return;
+  }
   if (req.method !== 'POST') {
     res.status(405).json({ error: 'Method not allowed' });
     return;
